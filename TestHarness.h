9a37184de993643e843fd6587a3bd5845b21de12
Cade Archer <ccarcher14@gmail.com>
2019-10-08 19:18:48 -0400

    Initial TestHarness header design

---
+++ TestHarness.h
@@ -0,0 +1,24 @@
+#include <list>
+#include <functional>
+using namespace std;
+class TestHarness
+{
+
+public:
+	
+	void execute(function<bool()> callable); //executes a callable at the set log level
+
+	enum LogLevel { HIGH, MEDIUM, LOW }; /* LOW: just pass-fail status
+											MEDIUM: application specific messages for pass and fail, along with the result
+											HIGH: detailed debugging output that includes, on failure, values of application specific variables, 
+											      and an optional time-date stamp
+										 */
+
+	void setLogLevel(LogLevel logLevel); //Sets the log level for the test Harness
+
+	void execute(list<function<bool()>> callables); //Executes a sequenece of callables at the set log level
+
+private:
+	LogLevel logLevel = HIGH;
+};
+
