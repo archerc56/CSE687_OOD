97a2217fa1e89d86c42124c137fd26cd61a2fcc4
Cade Archer <ccarcher14@gmail.com>
2019-10-10 20:41:54 -0400

    Add files via upload

---
+++ TestHarness.cpp
@@ -0,0 +1,40 @@
+#include "TestHarness.h"
+
+void TestHarness::setLogLevel(LogLevel logLevel)
+{
+
+}
+				
+void TestHarness::execute(std::function<void()> callable)
+{
+
+}
+		
+void TestHarness::execute(std::list<function<void()>> callables)
+{
+
+}
+
+
+void TestHarness::log(int)
+{
+
+}
+				
+
+std::string TestHarness::to_String()
+{
+
+}
+	
+
+void TestHarness::add_Test_to_Suite()
+{
+
+}
+	
+void TestHarness::reset_TestSuite()
+{
+
+}
+	
---
+++ TestHarness.h
@@ -0,0 +1,78 @@
+/**************************************************************************************************************************
+
+CIS 687 Object Oriented Design Monday 9 PM
+
+Authors:			
+
+File:			TestHarness.h
+
+Description:	
+
+Last Updated:	10/9/2019 7:08 PM
+
+
+//**************************************************************************************************************************/
+
+/**************************************************************************************************************************
+
+Notes:
+
+
+
+//**************************************************************************************************************************/
+#include <list>
+#include <functional>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class TestHarness
+{
+
+public:
+	TestHarness();
+	~TestHarness();
+
+	enum LogLevel { HIGH, MEDIUM, LOW };				/* LOW: just pass-fail status
+														MEDIUM: application specific messages for pass and fail, along with the result
+														HIGH: detailed debugging output that includes, on failure, values of application specific variables, 
+														 and an optional time-date stamp
+														*/
+
+	void setLogLevel(LogLevel logLevel);				//Sets the log level for the test Harness
+	void execute(std::function<void()> callable);		//executes a callable at the set log level
+	void execute(std::list<function<void()>> callables);//Executes a sequenece of callables at the set log level
+
+
+	void log(int);				//takes int arguement, 1 or 0 for pass or fail
+
+
+	std::string to_String();	//pretty much the getter for report
+
+	void add_Test_to_Suite();	//adds test case to testsuite
+	void reset_TestSuite();		//
+
+
+
+	//----Test Cases------
+	//void test_ReturnValue(T value);// if value is not expected the throw
+
+
+
+	//--------------------
+
+private:
+	LogLevel logLevel = HIGH;
+	std::vector<void(*)()> TestSuite;	//group of function pointers
+
+	std::stringstream report;			//final report of all passes and fails.  when log() is called this string is appended	
+
+
+	//----Test DATA-------
+
+
+
+	//--------------------
+};
+
