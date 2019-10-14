ed72cbf385c36cb9b101bc5b93dfd92153ef48c8
archercc <ccarcher14@gmail.com>
2019-10-14 18:19:51 -0400

    Update TestHarness.cpp

--- TestHarness.cpp
+++ TestHarness.cpp
@@ -2,42 +2,50 @@
 
 #include "TestHarness.h"
 
+TestHarness::TestHarness() {
+
+}
+
+TestHarness::~TestHarness() {
+
+}
 void TestHarness::setLogLevel(LogLevel logLevel)
 {
 
 }
-				
-void TestHarness::Executor(std::function<void()> callable)
-{
 
+bool TestHarness::executor(std::function<void()> callable)
+{
+	return false;
 }
 
 template <typename Callable>
-void TestHarness<Callable>::Executor(Callable& co)
+bool TestHarness::executor(Callable& co)
 {
-	
+	return false;
 }
-		
-void TestHarness::Executor(std::list<function<void()>> callables)
+
+bool TestHarness::executor(std::list<std::function<void()>> callables)
 {
+	return false;
 
 }
 
 void TestHarness::log(int)
 {
 
-}			
+}
 
 std::string TestHarness::to_String()
 {
-
+	return "";
 }
-	
+
 void TestHarness::add_Test_to_Suite()
 {
 
 }
-	
+
 void TestHarness::reset_TestSuite()
 {
 
