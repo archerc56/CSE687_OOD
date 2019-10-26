063dcb2e6b98f544faa9383787ccdac17405f84f
Michael Cheng <michael.cheng@intel.com>
2019-10-26 13:31:41 -0700

    Added xml parser and a test.xml file

--- TestHarness.cpp
+++ TestHarness.cpp
@@ -2,6 +2,62 @@
 #include <new>
 #include <memory>
 #include <typeinfo>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+// Remove substring from  a string 
+std::string removeSub(std::string s, std::string to_remove) 
+{
+    // Erase to_remove.lenght starting at the index where
+    // find found the first occurrence of the substring
+    using namespace std;
+    string::size_type size = s.find(to_remove);
+    if (size != string::npos)
+        s.erase(size, to_remove.length());
+
+    return s;
+}
+
+// Parse and load an xml file
+std::vector<std::string> ParseXML(std::string test_file)
+{
+    using namespace std;
+    ifstream inFile(test_file);
+    string input;
+    string to_parse = "<test_name>";
+    string to_remove = "</test_name>";
+    std::vector<string> dlls_raw_content;
+    std::vector<string> parsed_vector;
+
+    while (inFile)
+    {
+        getline(inFile, input);
+        dlls_raw_content.push_back(input);
+
+    }
+
+    inFile.close();
+
+    for (int i=0; i<dlls_raw_content.size(); i++) 
+    {
+        if (dlls_raw_content[i].find(to_parse)==0) 
+        {
+
+            string Final = removeSub(dlls_raw_content[i], to_parse);
+            Final = removeSub(Final, to_remove);
+            parsed_vector.push_back(Final);
+
+        }
+    }
+
+    for (int i=0; i<parsed_vector.size(); i++) 
+    {
+        cout << parsed_vector[i] << endl;
+    }
+    return parsed_vector;
+}
 
 TestHarness::TestHarness() {}
 TestHarness::~TestHarness() {}
@@ -139,6 +195,7 @@ void TestAllocate();
 
 int main()
 {
+    ParseXML("test.xml");
 
 	TestHarness th; //create instance of TestHarness class
 	//pass in various test functions into TestSuite. These functions trigger various exceptions.
